8123eba15ee23f67a18e9c31a1e69bcc50153b4e
codered072 <kali@ip-172-31-80-144.ec2.internal>
2023-07-02 00:20:23 +0000

    adds simple functionality

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, abort, Response
+import urllib, json
+
+app = Flask(__name__)
+
+@app.route("/forecast", methods=['GET'])
+def getWeather():
+    city = request.args.get('city')
+    if city is None:
+        abort(400, "No city provided")
+    
+    data = {}
+    data['q'] = request.args.get('city')
+    data['appid'] = '70d4fb5fc92bbe440ebb79dfb20c2f82'
+    data['units'] = 'metric'
+
+    url_values = urllib.parse.urlencode(data)
+    url = 'http://api.openweathermap.org/data/2.5/forecast'
+    full_url = url + '?' + url_values
+    data = urllib.request.urlopen(full_url)
+
+    resp = Response(data)
+    resp.status_code = 200
+    return render_template('index.html', title='The Weather Bee', data=json.loads(data.read().decode('utf-8')))
+
+
